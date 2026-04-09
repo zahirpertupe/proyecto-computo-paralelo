156c024bb346671fe95ddff87da9e7f1d6a58e19
ika-pul <149854700+ika-pul@users.noreply.github.com>
2026-04-08 19:47:27 -0600

    Update borradir.py

--- borradir.py
+++ borradir.py
@@ -2,10 +2,8 @@ import numpy as np
 from medmnist import FractureMNIST3D
 import multiprocessing as mp
 import random
-
-
 #esto es una prueba de edición
-
+#hola pirinola
 def Nivelar(n_p, num_pasos, matriz_datos):
     s = num_pasos % n_p    # residuo 
     t = num_pasos // n_p   # capas de cada procesador
